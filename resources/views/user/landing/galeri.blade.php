28c7a03e3f140e531fc65e527f5f25e1409ffd3c
Sulistyo Cahyo G <cariercahyo@gmail.com>
2023-02-28 08:21:03 +0900

    #

--- resources/views/user/landing/galeri.blade.php
+++ resources/views/user/landing/galeri.blade.php
@@ -20,9 +20,7 @@
     <div class="container">
 
     <div class="section-header">
-        <h2>Portfolio</h2>
-        <p>Non hic nulla eum consequatur maxime ut vero memo vero totam officiis pariatur eos dolorum sed fug dolorem est possimus esse quae repudiandae. Dolorem id enim officiis sunt deserunt esse soluta consequatur quaerat</p>
-    </div>
+        <h2>Galeri Kegiatan</h2>
 
     </div>
 
@@ -32,21 +30,17 @@
 
     <ul class="portfolio-flters">
         <li data-filter="*" class="filter-active">All</li>
-        <li data-filter=".filter-app">App</li>
-        <li data-filter=".filter-product">Product</li>
-        <li data-filter=".filter-branding">Branding</li>
-        <li data-filter=".filter-books">Books</li>
     </ul><!-- End Portfolio Filters -->
 
     <div class="row g-0 portfolio-container">
 
         @foreach ($post as $item)
         <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-app">
-        <img src="{{$item->thumbnail}}" class="img-fluid" alt="">
+        <img src="{{$item->thumbnail}}" class="img-fluid" alt="" style="min-height: 320px; max-height:320px; min-width:350px;max-width:350px">
         <div class="portfolio-info">
             <h4>App 1</h4>
             <a href="assets/img/portfolio/app-1.jpg" title="App 1" data-gallery="portfolio-gallery" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
-            <a href="portfolio-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+            <a href="{{route('blog.show', $item->id)}}" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
         </div>
         </div>
             
